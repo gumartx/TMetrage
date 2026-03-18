5129af542c303a6c3ef877263a645b9566596b3b
Gustavo Gomes <gustavo.gmartx@gmail.com>
2026-03-18 19:20:11 -0300

    Projeto criado

---
+++ backend/src/test/java/com/gusmarg/tmetrage/TmetrageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gusmarg.tmetrage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmetrageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
